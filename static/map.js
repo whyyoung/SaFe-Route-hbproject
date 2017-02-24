d22c3dbc1e24177715241d2a16304484b9f81edb
vagrant <vagrant@vagrant.vm>
2017-02-24 01:19:05 +0000

    multiple route handling with updated map markers working!

--- static/map.js
+++ static/map.js
@@ -21,27 +21,29 @@ startDate = (startDate.toISOString()).slice(0, 10)
 $("#submit").on('click', getDirections);
 // $("#submit").on('click', updateDatabase);
 
-
+var walkingRouteStored = []
 var directionsDisplay = new google.maps.DirectionsRenderer;
 // Takes a starting address and an ending address to build a visual walking route
 // on the map as well as written step-by-step directions to the right of the map.
 // When called, the old route with corresponding markers are removed to prepare the
 // map for a new route and set of corresponding markers/crime incidents.
+function setMapOnAll(map) {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+  }
+}
+
+function deleteMarkers(){
+  setMapOnAll(null);
+  markers = [];
+}
+
+
 function getDirections() {
-  function setMapOnAll(map) {
-    for (var i = 0; i < markers.length; i++) {
-      markers[i].setMap(map);
-    }
-  }
 
-  function deleteMarkers(){
-    setMapOnAll(null);
-    markers = [];
-  }
-
-  if (markers != null) {
-    deleteMarkers();
-  };
+    if (markers != null) {
+      deleteMarkers();
+    };
 
     directionsDisplay.setDirections({routes: []});
     var startAddress = document.getElementById('start-address').value;
@@ -59,24 +61,86 @@ function getDirections() {
     }, function(response, status) {
       if (status === 'OK') {
         directionsDisplay.setDirections(response);
-        var walkingRoute = response.routes[0]
-        makeBuffer(walkingRoute);
-      } else {
-        window.alert('Directions request failed due to ' + status);
-      }
+        var walkingRoute = response.routes[0].overview_path;
+        makeBuffer(walkingRoute);  
 
-    });
+        for(var i in response.routes) {
+            // var line = new google.maps.Polyline({
+            //   path: walkingRoute,
+            //   strokeColor: "FFEE00",  // you might want different colors per suggestion
+            //   strokeOpacity: 0.7,
+            //   strokeWeight: 3
+            // });
+            // line.setMap(map);
+            var obj = {};
+            obj[i] = response.routes[i].overview_path;
+            walkingRouteStored.push(obj);
+
+
+            // var line = drawPolyline(response.routes[i].overview_path, '#0000ff', hide);
+            // polylines.push(line);
+            // shadows.push(shadow);
+            // // let's add some data for the infoWindow
+            // data.push({
+            //     distance: response.routes[i].legs[0].distance,
+            //     duration: response.routes[i].legs[0].duration,
+            //     end_address: response.routes[i].legs[0].end_address,
+            //     start_address: response.routes[i].legs[0].start_address,
+            //     end_location: response.routes[i].legs[0].end_location,
+            //     start_location: response.routes[i].legs[0].start_location
+            // });
+            // bounds = line.getBounds(bounds);
+            // google.maps.event.addListener(shadow, 'click', function(e) {
+            //     // detect which route was clicked on
+            //     var index = shadows.indexOf(this);
+            //     highlightRoute(index, e);
+            }
+          }
+
+        });
+        // directionsDisplay.setDirections(response);
+                        // document.getElementsByClassName('adp-listinfo').addEventListener('click', alert("it worked!"));
+                      //   var onChangeHandler = function() {
+                      //     calculateAndDisplayRoute(directionsService, directionsDisplay);
+                      //   };
+                      //   document.getElementById('start').addEventListener('change', onChangeHandler);
+                      //   document.getElementById('end').addEventListener('change', onChangeHandler);
+                      // }
+      //   var walkingRoute = response.routes[0]
+      //   makeBuffer(walkingRoute);
+      // } else {
+      //   window.alert('Directions request failed due to ' + status);
+      // }
+
+    // });
+  debugger;
   }
-$("#right-panel").on('change', function() {
-    alert("Directions Changed!");
-  });
+// document.querySelector('body').addEventListener('click', function(event) {
+//   if (event.target.tagName.toLowerCase() === 'td') {
+
+//   console.log("hi");
+// }
+// });
+
+$('#right-panel').on('DOMNodeInserted', function(){
+  $("td").on('click', function(){
+    var x = this.dataset.routeIndex;
+    var newMarkers = walkingRouteStored[x];
+    if (markers != null) {
+      deleteMarkers();
+    };
+    makeBuffer(newMarkers[x]);
+    debugger;
+  })
+});
+
 
 // Creates url data get request to filter relevant crime incidents within 100 meters radius of
 // points along a given route (provided by Goggle Maps API). The request also filters
 // the data to provide reports made during the most current week available. Data received
 // is passed to makeMarkers function to place markers where incidents have occurred.
 function makeBuffer(route) {
-  var overviewPath = route.overview_path;
+  var overviewPath = route;
   for (i = 0; i < overviewPath.length; i++) {
      var lat = overviewPath[i].lat();
      var lng = overviewPath[i].lng();
@@ -112,7 +176,6 @@ function makeBuffer(route) {
 // info window pop-up upon marker click. Sets the markers onto the map, attaches info
 // window to each marker along with a listener to call the window.
 function makeMarker(data) {
-
   $.each(data, function(i, entry) {
       var longitude = parseFloat(entry["location"]["coordinates"][0])
       var latitude = parseFloat(entry["location"]["coordinates"][1])
@@ -131,7 +194,7 @@ function makeMarker(data) {
               position: newLatlng,
               map: map,
               title: entry["category"],
-              icon: image
+              // icon: image
             });
       markers.push(marker);
 
